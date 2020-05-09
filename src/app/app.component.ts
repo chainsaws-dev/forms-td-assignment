7ff33e88b32ff97cee659486fa3d3f3679bb0576
Vorobiev Alexander <chainsaws-dev@outlook.com>
2020-05-09 20:24:33 +0300

    Доделал задание

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ListOfOptions = ['Basic', 'Advanced', 'Pro'];
+  DefaultSubscription = 1;
+  SubmittedFormValue: Object;
 
+  @ViewChild('MainForm') submittedForm: NgForm;
+  OnSubmit() {
+    this.SubmittedFormValue = this.submittedForm.value;
+  }
 }
